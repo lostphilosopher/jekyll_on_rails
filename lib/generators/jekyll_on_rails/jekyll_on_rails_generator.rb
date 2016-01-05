1f9c7d3400524452b345015e223b55af32ca4efe
lostphilosopher <wandersen02@gmail.com>
2016-01-04 22:59:16 -0600

    WIP

--- lib/generators/jekyll_on_rails/jekyll_on_rails_generator.rb
+++ lib/generators/jekyll_on_rails/jekyll_on_rails_generator.rb
@@ -3,13 +3,13 @@ require 'rails/generators/base'
 module JekyllOnRails
   module Generators
     class JekyllOnRailsGenerator < Rails::Generators::Base
+      # @TODO MVC file should be here
       source_root File.expand_path("../../templates", __FILE__)
 
       desc 'Creates the necessary directories to hold blog posts'
       def create_directories
         Dir.mkdir('app/views/blog') unless Dir.exist?('app/views/blog')
         Dir.mkdir('app/views/blog/published') unless Dir.exist?('app/views/blog/published')
-        binding.pry
         copy_file("../../../config/views/index.html.haml", "app/views/blog/index.html.haml") unless File.exist?('app/views/blog/index.html.haml')
         copy_file("../../../config/controllers/blog_controller.rb", "app/controllers/blog_controller.rb") unless File.exist?('app/controllers/blog_controller.rb')
       end
